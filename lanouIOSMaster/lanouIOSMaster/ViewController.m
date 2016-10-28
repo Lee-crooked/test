6485a4fe59d7d30f0dd2cfdd2590a2f2082c65e6
Lee-crooked <470502584@qq.com>
2016-10-28 10:32:05 +0800

    create project
    create a new project

---
+++ lanouIOSMaster/lanouIOSMaster/ViewController.m
@@ -0,0 +1,35 @@
+//
+//  ViewController.m
+//  lanouIOSMaster
+//
+//  Created by lanou on 16/10/27.
+//  Copyright © 2016年 lanou. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.view.backgroundColor=[UIColor redColor];
+    
+    UIButton *btn=[UIButton buttonWithType:UIButtonTypeSystem];
+    
+    btn.frame=CGRectMake(0, 0, 50, 50);
+    [btn setTitle:@"baby" forState:UIControlStateNormal];
+    [self.view addSubview:btn];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
